ca9f94e107627c1e0102fb66b735c14b78dd3fe9
reutazaria <reutaz5@gmail.com>
2021-02-10 12:26:54 +0200

    add third graph of pearson

--- plotting.py
+++ plotting.py
@@ -3,7 +3,7 @@ import numpy as np
 import matplotlib.pyplot as plt
 import pandas as pd
 
-model_dir = '../pretrained_models/supervised/kitti/mode=dense.data=kitti.input=rgbd.resnet18.epochs5.criterion=l2.lr=0.0001.bs=2.wd=0.pretrained=True.jitter=0.1.time=2020-09-30@11-03'
+model_dir = '../results/mode=dense.data=nachsholim.input=rgbd.resnet18.epochs30.criterion=l2.lr=0.0001.bs=2.wd=0.pretrained=True.jitter=0.1.time=2021-02-09@15-42'
 train_filename = os.path.join(model_dir, 'train.csv')
 train_data = pd.read_csv(train_filename)
 epochs = train_data['epoch'].values
@@ -13,16 +13,11 @@ smooth_loss = train_data['smooth_loss'].values
 photometric_loss = train_data['photometric_loss'].values
 train_error = train_data['rmse'].values
 epoch_num = len(epochs)
-# print('Best train loss is: {:.3f}, in epoch {:d}/{:d}'.format(min(train_loss),
-#                                                               min(np.where(train_loss == min(train_loss))[0]) + 1,
-#                                                               epoch_num))
 
 val_filename = os.path.join(model_dir, 'val.csv')
 val_data = pd.read_csv(val_filename)
 val_error = val_data['rmse'].values
-# print('Best validation error is: {:.3f}, in epoch {:d}/{:d}'.format(min(val_error),
-#                                                                     min(np.where(val_error == min(val_error))[0]) + 1,
-#                                                                     epoch_num))
+val_pearson = val_data['pearson'].values
 
 fig0 = plt.figure(1)
 data_set = model_dir.split('data=')[1].split('.')[0]
@@ -34,18 +29,24 @@ epochs = range(epoch_num) + np.ones(epoch_num)
 epoch_num = min(epoch_num, 35)
 epochs = range(1, epoch_num+1)
 
-plt.subplot(121)
+plt.subplot(131)
 plt.plot(epochs, depth_loss[0:epoch_num], label='Depth Loss')
-min_ind = np.argmin(depth_loss)
-plt.text(epochs[min_ind], depth_loss[min_ind], "%.3f" %depth_loss[min_ind], ha="center",
-         bbox=dict(facecolor='grey', alpha=0.5))
-if '+photo' in mode:
+min_ind = np.argmin(depth_loss[0:epoch_num])
+plt.plot(epochs[min_ind], depth_loss[min_ind], 'xk')
+plt.annotate("({:d},{:.3f})".format(epochs[min_ind], depth_loss[min_ind]),
+            (epochs[min_ind], depth_loss[min_ind]), ha="center", bbox=dict(facecolor='grey', alpha=0.5))
+if '+' in mode:
     plt.plot(epochs, train_loss[0:epoch_num], label='Train Loss')
-    min_ind = np.argmin(train_loss)
-    plt.text(epochs[min_ind], train_loss[min_ind], "%.3f" % train_loss[min_ind], ha="center",
-             bbox=dict(facecolor='grey', alpha=0.5))
+    min_ind = np.argmin(train_loss[0:epoch_num])
+    plt.plot(epochs[min_ind], train_loss[min_ind], 'xk')
+    plt.annotate("({:d},{:.3f})".format(epochs[min_ind], train_loss[min_ind]),
+                 (epochs[min_ind], train_loss[min_ind]), ha="center", bbox=dict(facecolor='grey', alpha=0.5))
     plt.plot(epochs, smooth_loss[0:epoch_num], label='Smooth Loss')
-    plt.plot(epochs, photometric_loss[0:epoch_num], label='Photometric Loss')
+    plt.plot(epochs, photometric_loss[0:epoch_num], label='Correlation Loss')
+    min_ind = np.argmin(photometric_loss[0:epoch_num])
+    plt.plot(epochs[min_ind], photometric_loss[min_ind], 'xk')
+    plt.annotate("({:d},{:.3f})".format(epochs[min_ind], photometric_loss[min_ind]),
+                 (epochs[min_ind], photometric_loss[min_ind]), ha="center", bbox=dict(facecolor='grey', alpha=0.5))
 plt.legend(loc='upper right')
 plt.xlabel('Epoch')
 plt.ylabel('Losses')
@@ -55,24 +56,38 @@ print('Best train loss is: {:.3f}, in epoch {:d}/{:d}'.format(min(train_loss),
                                                               min(np.where(train_loss == min(train_loss))[0]) + 1,
                                                               epoch_num))
 
-plt.subplot(122)
+plt.subplot(132)
 val_error_trim = val_error[0:epoch_num]
-plt.plot(epochs, val_error_trim, label='Validation Error')
+plt.plot(epochs, val_error_trim, label='RMSE')
 min_ind = np.argmin(val_error_trim)
 plt.plot(epochs[min_ind], val_error_trim[min_ind], 'xk')
-plt.text(epochs[min_ind], val_error_trim[min_ind], "%.3f" %val_error_trim[min_ind], ha="right",
-         bbox=dict(facecolor='grey', alpha=0.5))
+plt.annotate("({:d},{:.3f})".format(epochs[min_ind], val_error_trim[min_ind]),
+             (epochs[min_ind], val_error_trim[min_ind]), ha="center", bbox=dict(facecolor='grey', alpha=0.5))
 plt.legend(loc='upper right')
 plt.xlabel('Epoch')
 plt.ylabel('RMSE [mm]')
-plt.title('Validation Error')
+plt.title('Validation RMSE')
 plt.grid(True)
-plt.show()
 print('Best validation error is: {:.3f}, in epoch {:d}/{:d}'.format(val_error_trim[min_ind],
                                                                     epochs[min_ind],
                                                                     epoch_num))
 
-fig0.set_size_inches(10, 5)
+plt.subplot(133)
+val_pearson_trim = val_pearson[0:epoch_num]
+plt.plot(epochs, val_pearson_trim, label='Pearson')
+max_ind = np.argmax(val_pearson_trim)
+plt.plot(epochs[max_ind], val_pearson_trim[max_ind], 'xk')
+plt.annotate("({:d},{:.3f})".format(epochs[max_ind], val_pearson_trim[max_ind]),
+             (epochs[max_ind], val_pearson_trim[max_ind]), ha="center", va="bottom", bbox=dict(facecolor='grey', alpha=0.5))
+plt.legend(loc='lower right')
+plt.xlabel('Epoch')
+plt.ylabel('Pearson')
+plt.title(r'Validation $\rho$')
+plt.grid(True)
+
+fig0.set_size_inches(20, 5)
+plt.show()
+
 fig0.savefig(model_dir + '/' + 'train_loss.png')
 
 
