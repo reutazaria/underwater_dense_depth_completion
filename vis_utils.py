a73411eef6242cd8550bb1b426179107b97af431
reutazaria <reutaz5@gmail.com>
2021-02-10 12:25:33 +0200

    diff row as comment and  larfer dilation

--- vis_utils.py
+++ vis_utils.py
@@ -36,14 +36,14 @@ def merge_into_col(ele, pred):
     # add depth images
     if 'd' in ele:
         d_im = preprocess_depth(ele['d'][0, ...])
-        d_im_dilate = cv2.dilate(d_im, np.ones((3, 3), np.uint8), iterations=1)
+        d_im_dilate = cv2.dilate(d_im, np.ones((5, 5), np.uint8), iterations=1)
         depth_im = np.concatenate((d_im_dilate, depth_im), axis=0)
     if 'gt' in ele:
         gt_im = preprocess_depth(ele['gt'][0, ...])
         depth_im = np.concatenate((depth_im, gt_im), axis=0)
-        diff_im = gt_im - preprocess_depth(pred[0, ...])
-        diff_im[gt_im == 0] = 0
-        depth_im = np.concatenate((depth_im, diff_im), axis=0)
+        # diff_im = gt_im - preprocess_depth(pred[0, ...])
+        # diff_im[gt_im == 0] = 0
+        # depth_im = np.concatenate((depth_im, diff_im), axis=0)
     img_list.append(depth_colorize(depth_im))
 
     # if 'd' in ele:
