418e0123e21acbeed65372bd057b1c5095829279
reutazaria <reutaz5@gmail.com>
2021-05-06 20:01:36 +0300

    rearrange lines

--- criteria.py
+++ criteria.py
@@ -110,10 +110,8 @@ class SmoothnessLoss(nn.Module):
             assert x.dim(
             ) == 4, "expected 4-dimensional data, but instead got {}".format(
                 x.dim())
-            horizontal = 2 * x[:, :, 1:-1, 1:-1] - x[:, :, 1:-1, :
-                                                                 -2] - x[:, :, 1:-1, 2:]
-            vertical = 2 * x[:, :, 1:-1, 1:-1] - x[:, :, :-2, 1:
-                                                              -1] - x[:, :, 2:, 1:-1]
+            horizontal = 2 * x[:, :, 1:-1, 1:-1] - x[:, :, 1:-1, :-2] - x[:, :, 1:-1, 2:]
+            vertical = 2 * x[:, :, 1:-1, 1:-1] - x[:, :, :-2, 1:-1] - x[:, :, 2:, 1:-1]
             der_2nd = horizontal.abs() + vertical.abs()
             return der_2nd.mean()
 
